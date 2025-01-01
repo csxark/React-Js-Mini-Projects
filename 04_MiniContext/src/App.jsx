bfc3269d94010961d31c0c8ac72a2d26c71a0776
csxark <arktandoncs@gmail.com>
2025-01-01 20:04:04 +0530

    Update App.jsx

--- 04_MiniContext/src/App.jsx
+++ 04_MiniContext/src/App.jsx
@@ -8,9 +8,10 @@ function App() {
 
   return (
     <UserContextProvider>
+      <section className='flex flex-col justify-center items-center h-screen bg-slate-700 text-white'>
       <h1>React with Chai and share is important</h1>
       <Login />
-      <Profile />
+      <Profile /></section>
     </UserContextProvider>
   )
 }
