c1bd209f70296e5f8685d10121e2876f5756c61e
csxark <arktandoncs@gmail.com>
2025-02-16 16:50:30 +0530

    ems

--- 09_EmploymentMangementSystem/src/App.jsx
+++ 09_EmploymentMangementSystem/src/App.jsx
@@ -1,11 +1,50 @@
-import { } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import Login from './components/Auth/Login'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
+import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { AuthContext } from './Context/AuthProvider'
+
+const App = () => {
+
+  const [user, setUser] = useState(null)
+  const [loggedInUserData, setLoggedInUserData] = useState(null)
+  const [userData, SetUserData] = useContext(AuthContext)
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem('loggedInUser')
+
+    if (loggedInUser) {
+      const userData = JSON.parse(loggedInUser)
+      setUser(userData.role)
+      setLoggedInUserData(userData.data)
+    }
+
+  }, [])
 
 
-function App() {
+  const handleLogin = (email, password) => {
+    if (email == 'admin@me.com' && password == '123') {
+      setUser('admin')
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
+    } else if (userData) {
+      const employee = userData.find((e) => email == e.email && e.password == password)
+      if (employee) {
+        setUser('employee')
+        setLoggedInUserData(employee)
+        localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee', data: employee }))
+      }
+    }
+    else {
+      alert("Invalid Credentials")
+    }
+  }
+
+
 
   return (
     <>
-      
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user == 'admin' ? <AdminDashboard changeUser={setUser} /> : (user == 'employee' ? <EmployeeDashboard changeUser={setUser} data={loggedInUserData} /> : null)}
     </>
   )
 }
